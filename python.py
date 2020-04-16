2c30aa8a2c80077fc325391026a65bee97159984
sunny092318 <sunny.prakash1097@gmail.com>
2020-04-16 11:32:07 +0000

    first commit

---
+++ python.py
@@ -0,0 +1,8 @@
+import subprocess
+process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
+output = process.communicate()[0]
+print ('.')
+print('.')
+print ('ths is just for learning purposes')  #only for use case
+print("Line A \n Line B")
+open("log.txt", "a")
