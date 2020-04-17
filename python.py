19421ed37248b1a85f1ae39333eaff5f98bb8263
sunny092318 <sunny.prakash1097@gmail.com>
2020-04-17 14:26:14 +0000

    first commit

--- python.py
+++ python.py
@@ -8,5 +8,5 @@ print("Line A \n Line B")
 <<<<<<< HEAD
 open("log.txt", "abc")
 =======
-open("log.txt", "abcdesssssspppppppppppp")
+open("log.txt", "abcdesssspppppppppppp")
 >>>>>>> b2b850e0d3ab2eaab83fc54867e97a8906d336d8
